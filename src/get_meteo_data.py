609d01a9f5830b44a22c8429a9700a389da1d4d7
Ny Hasina Vagno <hasina.vagno@esiroi.re>
2025-12-23 23:15:31 +0400

    chore: merge meteo data with velib

--- src/get_meteo_data.py
+++ src/get_meteo_data.py
@@ -1,52 +1,57 @@
-import openmeteo_requests
+if __name__ == '__main__':
+    import openmeteo_requests
 
-import pandas as pd
-import requests_cache
-from retry_requests import retry
+    import pandas as pd
+    import requests_cache
+    from retry_requests import retry
 
-cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
-retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-openmeteo = openmeteo_requests.Client(session=retry_session)
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
 
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+        "latitude": 48.8534,
+        "longitude": 2.3488,
+        "start_date": "2025-10-15",
+        "end_date": "2025-12-23",
+        "daily": ["precipitation_sum", "rain_sum", "snowfall_sum", "sunrise", "sunset"],
+        "hourly": ["temperature_2m", "precipitation", "rain", "snowfall", "relative_humidity_2m"],
+        "timezone": "Europe/London",
+    }
+    responses = openmeteo.weather_api(url, params=params)
 
-url = "https://archive-api.open-meteo.com/v1/archive"
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation: {response.Elevation()} m asl")
+    print(f"Timezone: {response.Timezone()}{response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
 
-params = {
-    "latitude": 48.8534,
-    "longitude": 2.3488,
-    "start_date": "2024-10-19",
-    "end_date": "2024-12-20",
-    "daily": ["temperature_2m_mean", "precipitation_sum", "rain_sum", "snowfall_sum", "sunrise", "sunset"],
-    "timezone": "Europe/London",
-}
-responses = openmeteo.weather_api(url, params=params)
+    # Process hourly data. The order of variables needs to be the same as requested.
+    hourly = response.Hourly()
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+    hourly_precipitation = hourly.Variables(1).ValuesAsNumpy()
+    hourly_rain = hourly.Variables(2).ValuesAsNumpy()
+    hourly_snowfall = hourly.Variables(3).ValuesAsNumpy()
+    hourly_relative_humidity_2m = hourly.Variables(4).ValuesAsNumpy()
 
-response = responses[0]
-print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
-print(f"Elevation: {response.Elevation()} m asl")
-print(f"Timezone: {response.Timezone()}{response.TimezoneAbbreviation()}")
-print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+    hourly_data = {"date": pd.date_range(
+        start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+        end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+        freq=pd.Timedelta(seconds=hourly.Interval()),
+        inclusive="left"
+    )}
 
-daily = response.Daily()
-daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
-daily_precipitation_sum = daily.Variables(1).ValuesAsNumpy()
-daily_rain_sum = daily.Variables(2).ValuesAsNumpy()
-daily_snowfall_sum = daily.Variables(3).ValuesAsNumpy()
-daily_sunrise = daily.Variables(4).ValuesInt64AsNumpy()
-daily_sunset = daily.Variables(5).ValuesInt64AsNumpy()
+    hourly_data["temperature_2m"] = hourly_temperature_2m
+    hourly_data["precipitation"] = hourly_precipitation
+    hourly_data["rain"] = hourly_rain
+    hourly_data["snowfall"] = hourly_snowfall
+    hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
 
-daily_data = {"date": pd.date_range(
-    start=pd.to_datetime(daily.Time(), unit="s", utc=True),
-    end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
-    freq=pd.Timedelta(seconds=daily.Interval()), inclusive="left"),
-    "temperature_2m_mean": daily_temperature_2m_mean,
-    "precipitation_sum": daily_precipitation_sum,
-    "rain_sum": daily_rain_sum,
-    "snowfall_sum": daily_snowfall_sum,
-    "sunrise": daily_sunrise,
-    "sunset": daily_sunset
-}
+    hourly_dataframe = pd.DataFrame(data=hourly_data)
 
-daily_dataframe = pd.DataFrame(data=daily_data)
-
-daily_dataframe.to_csv("../data/meteo/daily_weather_2024_10_19_to_2024_12_20.csv", index=False)
+    hourly_dataframe.to_csv("../data/meteo/daily_weather_2025_12_15_to_2025_12_23.csv", index=False)
